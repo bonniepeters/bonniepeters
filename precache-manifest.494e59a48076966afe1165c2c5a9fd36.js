1aa71b79f9329f0a734446290903d0e829cc54db
Bonnie Peters <bonniepeters@Bonnies-MacBook-Pro.local>
2019-11-25 09:02:32 -0700

    Updates

---
+++ precache-manifest.494e59a48076966afe1165c2c5a9fd36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e622054cad826c0848024cabac4e567",
+    "url": "/bonniepeters/index.html"
+  },
+  {
+    "revision": "c82f9fac6b7d85dbe2e9",
+    "url": "/bonniepeters/static/css/main.0eb182eb.chunk.css"
+  },
+  {
+    "revision": "26e27b62a0f14cf555a6",
+    "url": "/bonniepeters/static/js/2.f94d2c26.chunk.js"
+  },
+  {
+    "revision": "c82f9fac6b7d85dbe2e9",
+    "url": "/bonniepeters/static/js/main.d551bb76.chunk.js"
+  },
+  {
+    "revision": "c11ff1b7afe612b8f574",
+    "url": "/bonniepeters/static/js/runtime-main.445418b9.js"
+  }
+]);
