8ed761bb8ba07b646aa9a817b62eb2d7f2e2d46c
bonniepeters <bonnie.e.peters@gmail.com>
2019-11-25 16:05:25 -0700

    more changes to about

--- bonnie-peters-portfolio/src/Components/About/About.js
+++ bonnie-peters-portfolio/src/Components/About/About.js
@@ -5,7 +5,7 @@ class About extends Component {
     return (
       <div className="about">
         <h2>About</h2>
-        <p>Hello, I’m Bonnie. I’m a front end developer, traveler, and National Geographic subscriber. Thanks to my grandmother and her library of National Geographic issues when I was growing up, I developed a relentless curiosity to explore different cultures and subjects outside my surrounding world. As an adult, I continue to subscribe to National Geographic and apply what I have learned in those pages about the diversity of humanity to the way I code. I always strive to build applications that speak to people who see things differently than I do. Whatever it takes to make that product, whether its learning a new algorithm, framework, or language, I'm up for the challenge!</p>
+        <p>Hello, I’m Bonnie. I’m a front end developer, traveler, and National Geographic subscriber. Thanks to my grandmother and her library of National Geographic issues when I was growing up, I developed a relentless curiosity to explore different cultures and subjects outside my surrounding world. Years later, I continue to subscribe to National Geographic and apply what I have learned in those pages about the diversity of humanity to the way I code. I always strive to build applications that speak to people who might see things differently than I do. Whatever it takes to make that product, whether its learning a new algorithm, framework, or language, I'm up for the challenge!</p>
       </div>
     );
   }
